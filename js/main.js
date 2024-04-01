491841c9ff00122d7aed65b0e74245368a517f90
J0RF4R <165432305+J0RF4R@users.noreply.github.com>
2024-03-31 20:46:38 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+dycalendar.draw({
+    target: '#dycalendar',
+    type: 'month',
+    dayformat:'full',
+    monthformat: 'full',
+    dayformat: 'full',
+    highlighttoday: true,
+    highlighttargetdates: true,
+    prevnextbutton: 'show',
+    dates: [
+        { month: 10, date: 29 }, 
+        { month: 10, date: 4 }, 
+        { month: 5, date: 10 }, 
+        { month: 3, date: 20 },
+        { month: 2, date: 4 },
+        { month: 7, date: 9 },
+        { month: 8, date: 1 },
+        { month: 9, date: 1 },
+        { month: 5, date: 5 },
+        { month: 8, date: 19 },
+        { month: 9, date: 23 },
+        { month: 2, date: 12 },
+        { month: 1, date: 15 },
+
+    ]
+})
